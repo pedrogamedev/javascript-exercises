05aebbfff23782f66283b7dead1633bc97e2ef94
pedrogamedev <pedrobrunigavioli@gmail.com>
2023-06-07 17:17:55 -0300

    Exercise 06 Completed!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
 
+    if(start < 0 || finish < 0)
+    {
+        return "ERROR";
+    }
+    if(!Number.isInteger(start) || !Number.isInteger(finish))
+    {
+        return "ERROR";
+    }
+    let result = 0;
+
+
+    if(start < finish)
+    {
+        for(i = start; i<= finish; i++)
+        {
+            result += i;
+        }
+    }
+    else
+    {
+        for(i = finish; i<= start; i++)
+        {
+            result += i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
