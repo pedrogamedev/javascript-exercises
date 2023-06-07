b093bdcb92fdeb3b01ffad243d926b5c649f7558
pedrogamedev <pedrobrunigavioli@gmail.com>
2023-06-07 16:07:49 -0300

    Exercises 01 and 02 Completed!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, loops) {
 
+    if(loops < 0)
+    {
+        return "ERROR";
+    }
+    let result = "";
+    for(i = 0; i<loops; i++)
+    {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
