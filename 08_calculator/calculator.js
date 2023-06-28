7528c9f86d9e3395736ad969fe9d125f088779b0
pedrogamedev <pedrobrunigavioli@gmail.com>
2023-06-28 14:18:47 -0300

    Exercise 08 - Completed!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, current) => sum + current, 0);
+
 };
 
 const multiply = function() {
-
+    let result = 1;
+  for(i = 0; i < arguments.length; i++)
+  {
+    result *= arguments[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let result = 1; for(i = 1; i <= b; i++)
+  {
+   result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
+const factorial = function(a) {
 	
+  let result = 1; for(i = 1; i <= a; i++)
+  {
+   result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
