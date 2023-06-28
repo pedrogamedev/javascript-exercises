9966ae657ab1980869ee936b02097d066a429d6d
pedrogamedev <pedrobrunigavioli@gmail.com>
2023-06-28 15:32:42 -0300

    Exercise 10 - Completed!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let result =1;
+    let lastResult = 0;
+    
+    if(n <0)
+    {
+        return "OOPS";
+    }
+    if(n == 1)
+    {
+        return result;
+    }
+    for(i = 1; i< n; i++)
+    {
+        let tmp = result;
+        result = lastResult + result;
+        lastResult = tmp;
+    }
+    return result;
 };
 
 // Do not edit below this line
