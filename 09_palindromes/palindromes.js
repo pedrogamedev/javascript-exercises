8e1b59af935113582c9ce1fd4fab6a2427100f76
pedrogamedev <pedrobrunigavioli@gmail.com>
2023-06-29 11:29:41 -0300

    Exercise 11 - Completed!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,33 @@
-const palindromes = function () {
+const palindromes = function (arr) {
+    arr = arr.split('');
+    arr.forEach( element,index => {
+        if(element == '!' || element == '?' || element == '.' || element == ' '|| element == ',')
+        {
+            arr = arr.splice(index, 1);
+        }
+    });
 
+    rra = arr.reverse();
+    arr = arr.reduce((sum, current) => sum + current,'');
+
+    rra = rra.reduce((sum, current) => sum + current, '');
+
+
+    rra = rra.toLowerCase();
+
+    arr = arr.toLowerCase();
+
+    console.log(arr);
+
+    console.log(rra);
+    if(arr === rra)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 };
 
 // Do not edit below this line
