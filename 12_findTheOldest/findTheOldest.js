8ae4dc9bb4ea766205cd0bb6eddc94b191165b96
pedrogamedev <pedrobrunigavioli@gmail.com>
2023-06-29 11:54:34 -0300

    Exercise 12 - Completed!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldestAge = 0;
+    let curAge = 0;
+    let oldestPerson;
 
+    let date = new Date().getFullYear()
+    arr.forEach(element => {
+        if(element.yearOfDeath != undefined)
+        {
+            curAge = element.yearOfDeath - element.yearOfBirth;
+        }
+        else
+        {
+            curAge = date - element.yearOfBirth;
+        }
+        console.log(curAge);
+        if(curAge > oldestAge)
+        {
+            oldestPerson = element;
+            oldestAge = curAge;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
